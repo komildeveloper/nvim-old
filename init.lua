f16acf95a58ffa395eea7466b9402e4fe21df42a
komildeveloper <komildeveloper111@gmail.com>
2021-06-19 19:04:20 +0500

    require some more plugins

--- init.lua
+++ init.lua
@@ -1,6 +1,17 @@
 require('plugins')
 require('keymappings')
 require('settings')
-require('nv-compe')
-require('lsp.lua-ls')
 require('colorscheme')
+
+-- Plugins
+require('nv-treesitter')
+require('nv-compe')
+require('nv-nvimtree')
+require('nv-colorizer')
+require('nv-galaxyline')
+require('nv-bufferline')
+require('nv-gitsigns')
+require('nv-nvim-autopairs')
+
+-- LSP
+require('lsp.lua-ls')
