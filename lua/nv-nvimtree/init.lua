e900148c986a9a4c85c7c84dae6cb7ca92b643e5
komildeveloper <komildeveloper111@gmail.com>
2021-07-04 01:59:09 +0500

    autoformat for nv-folders

--- lua/nv-nvimtree/init.lua
+++ lua/nv-nvimtree/init.lua
@@ -40,48 +40,46 @@ let g:nvim_tree_show_icons = {
 "if nvim-web-devicons is installed and on your runtimepath.
 "if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
 "but this will not work when you set indent_markers (because of UI conflict)
- ]]
-
-vim.g.nvim_tree_disable_netrw = 0 -- 1 by default, disables netrw
+ ]] vim.g.nvim_tree_disable_netrw = 0 -- 1 by default, disables netrw
 vim.g.nvim_tree_hijack_netrw = 0 -- 1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
 vim.g.nvim_tree_hide_dotfiles = 1 -- 0 by default, this option hides files and folders starting with a dot `.`
 vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
-vim.g.nvim_tree_follow = 1 --0 by default, this option allows the cursor to be updated when entering a buffer
-vim.g.nvim_tree_auto_close = 1 --0 by default, closes the tree when it's the last window
+vim.g.nvim_tree_follow = 1 -- 0 by default, this option allows the cursor to be updated when entering a buffer
+vim.g.nvim_tree_auto_close = 1 -- 0 by default, closes the tree when it's the last window
 vim.g.nvim_tree_auto_ignore_ft = 'startify' -- empty by default, don't auto open tree on specific filetypes.
 -- vim.g.nvim_tree_ignore = [ '.git', 'node_modules', '.cache' ] --empty by default
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-    vim.g.nvim_tree_bindings = {
-      -- default mappings
-      ["<CR>"]           = tree_cb("edit"),
-      ["o"]              = tree_cb("edit"),
-      ["<2-LeftMouse>"]  = tree_cb("edit"),
-      ["<2-RightMouse>"] = tree_cb("cd"),
-      ["<C-]>"]          = tree_cb("cd"),
-      ["<C-v>"]          = tree_cb("vsplit"),
-      ["<C-x>"]          = tree_cb("split"),
-    ["<C-t>"]          = tree_cb("tabnew"),
-      ["<"]              = tree_cb("prev_sibling"),
-      [">"]              = tree_cb("next_sibling"),
-      ["<BS>"]           = tree_cb("close_node"),
-      ["<S-CR>"]         = tree_cb("close_node"),
-      ["<Tab>"]          = tree_cb("preview"),
-      ["I"]              = tree_cb("toggle_ignored"),
-      ["H"]              = tree_cb("toggle_dotfiles"),
-      ["R"]              = tree_cb("refresh"),
-      ["a"]              = tree_cb("create"),
-      ["d"]              = tree_cb("remove"),
-      ["r"]              = tree_cb("rename"),
-      ["<C-r>"]          = tree_cb("full_rename"),
-      ["x"]              = tree_cb("cut"),
-      ["c"]              = tree_cb("copy"),
-      ["p"]              = tree_cb("paste"),
-      ["y"]              = tree_cb("copy_name"),
-      ["Y"]              = tree_cb("copy_path"),
-      ["gy"]             = tree_cb("copy_absolute_path"),
-      ["[c"]             = tree_cb("prev_git_item"),
-      ["]c"]             = tree_cb("next_git_item"),
-      ["-"]              = tree_cb("dir_up"),
-      ["q"]              = tree_cb("close"),
-    }
+vim.g.nvim_tree_bindings = {
+    -- default mappings
+    ["<CR>"] = tree_cb("edit"),
+    ["o"] = tree_cb("edit"),
+    ["<2-LeftMouse>"] = tree_cb("edit"),
+    ["<2-RightMouse>"] = tree_cb("cd"),
+    ["<C-]>"] = tree_cb("cd"),
+    ["<C-v>"] = tree_cb("vsplit"),
+    ["<C-x>"] = tree_cb("split"),
+    ["<C-t>"] = tree_cb("tabnew"),
+    ["<"] = tree_cb("prev_sibling"),
+    [">"] = tree_cb("next_sibling"),
+    ["<BS>"] = tree_cb("close_node"),
+    ["<S-CR>"] = tree_cb("close_node"),
+    ["<Tab>"] = tree_cb("preview"),
+    ["I"] = tree_cb("toggle_ignored"),
+    ["H"] = tree_cb("toggle_dotfiles"),
+    ["R"] = tree_cb("refresh"),
+    ["a"] = tree_cb("create"),
+    ["d"] = tree_cb("remove"),
+    ["r"] = tree_cb("rename"),
+    ["<C-r>"] = tree_cb("full_rename"),
+    ["x"] = tree_cb("cut"),
+    ["c"] = tree_cb("copy"),
+    ["p"] = tree_cb("paste"),
+    ["y"] = tree_cb("copy_name"),
+    ["Y"] = tree_cb("copy_path"),
+    ["gy"] = tree_cb("copy_absolute_path"),
+    ["[c"] = tree_cb("prev_git_item"),
+    ["]c"] = tree_cb("next_git_item"),
+    ["-"] = tree_cb("dir_up"),
+    ["q"] = tree_cb("close")
+}
--- lua/nv-utils/init.lua
+++ lua/nv-utils/init.lua
@@ -8,23 +8,23 @@ local function define_groups(definitions) -- {{{1
     --   3. Text
     -- just like how they would normally be defined from Vim itself
     for group_name, definition in pairs(definitions) do
-	vim.cmd('augroup' .. group_name)
-	vim.cmd('autocmd!')
+        vim.cmd('augroup' .. group_name)
+        vim.cmd('autocmd!')
 
-	for _, def in pairs(definition) do
-	    local command = table.concat(vim.tbl_flatten {'autocmd', def}, ' ')
-	    vim.cmd(command)
-	end
+        for _, def in pairs(definition) do
+            local command = table.concat(vim.tbl_flatten {'autocmd', def}, ' ')
+            vim.cmd(command)
+        end
 
-	vim.cmd('augroup END')
+        vim.cmd('augroup END')
     end
 end
 
-define_groups(
-    { _general_settings = {
-	    {
-	      'TextYankPost', '*', 'lua require(\'vim.highlight\').on_yank({higroup = \'IncSearch\', timeout})'
-            },
-       },
+define_groups({
+    _general_settings = {
+        {
+            'TextYankPost', '*',
+            'lua require(\'vim.highlight\').on_yank({higroup = \'IncSearch\', timeout})'
+        }
     }
-)
+})
