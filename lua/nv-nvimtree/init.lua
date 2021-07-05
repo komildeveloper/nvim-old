b907205d1f09e95e3da664c9b6e8118b7538a886
komildeveloper <komildeveloper111@gmail.com>
2021-07-05 22:49:12 +0500

    nvim-tree update

--- lua/nv-nvimtree/init.lua
+++ lua/nv-nvimtree/init.lua
@@ -48,6 +48,8 @@ vim.g.nvim_tree_follow = 1 -- 0 by default, this option allows the cursor to be 
 vim.g.nvim_tree_auto_close = 1 -- 0 by default, closes the tree when it's the last window
 vim.g.nvim_tree_auto_ignore_ft = 'startify' -- empty by default, don't auto open tree on specific filetypes.
 -- vim.g.nvim_tree_ignore = [ '.git', 'node_modules', '.cache' ] --empty by default
+vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
+
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_bindings = {
