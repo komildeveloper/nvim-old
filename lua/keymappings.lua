c9d19dd4afeaeb8b9028f0d906af91a3957b4306
komildeveloper <komildeveloper111@gmail.com>
2021-07-05 19:36:08 +0500

    update keys file

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,6 +1,5 @@
 vim.api
     .nvim_set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true})
-vim.g.mapleader = ' '
 
 -- no hl
 vim.api.nvim_set_keymap('n', '<Leader>h', ':set hlsearch!<CR>',
