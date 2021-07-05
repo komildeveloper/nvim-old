7650895960823bd3b7e1d8fccad1a11564188bd0
komildeveloper <komildeveloper111@gmail.com>
2021-07-06 00:53:18 +0500

    update nvimtree

--- lua/nv-nvimtree/init.lua
+++ lua/nv-nvimtree/init.lua
@@ -2,12 +2,9 @@
 let g:nvim_tree_side = 'right' "left by default
 let g:nvim_tree_width = 40 "30 by default
 let g:nvim_tree_gitignore = 1 "0 by default
-let g:nvim_tree_auto_open = 1 "0 by default, opens the tree when typing `vim $DIR` or `vim`
-let g:nvim_tree_quit_on_open = 1 "0 by default, closes the tree when you open a file
 let g:nvim_tree_git_hl = 1 "0 by default, will enable file highlight for git attributes (can be used without the icons).
 let g:nvim_tree_highlight_opened_files = 1 "0 by default, will enable folder and file icon highlight for opened files/directories.
 let g:nvim_tree_root_folder_modifier = ':~' "This is the default. See :help filename-modifiers for more options
-let g:nvim_tree_tab_open = 1 "0 by default, will open the tree when entering a new tab and the tree was previously open
 let g:nvim_tree_width_allow_resize  = 1 "0 by default, will not resize the tree when opening a file
 let g:nvim_tree_add_trailing = 1 "0 by default, append a trailing slash to folder names
 let g:nvim_tree_group_empty = 1 " 0 by default, compact folders that only contain a single folder into one node in the file tree
@@ -28,28 +25,32 @@ let g:nvim_tree_window_picker_exclude = {
 " Dictionary of buffer option names mapped to a list of option values that
 " indicates to the window picker that the buffer's window should not be
 " selectable.
-let g:nvim_tree_special_files = [ 'README.md', 'Makefile', 'MAKEFILE' ] " List of filenames that gets highlighted with NvimTreeSpecialFile
-let g:nvim_tree_show_icons = {
-    \ 'git': 1,
-    \ 'folders': 0,
-    \ 'files': 0,
-    \ 'folder_arrows': 0,
-    \ }
 "If 0, do not show the icons for one of 'git' 'folder' and 'files'
 "1 by default, notice that if 'files' is 1, it will only display
 "if nvim-web-devicons is installed and on your runtimepath.
 "if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
 "but this will not work when you set indent_markers (because of UI conflict)
- ]] vim.g.nvim_tree_disable_netrw = 0 -- 1 by default, disables netrw
-vim.g.nvim_tree_hijack_netrw = 0 -- 1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
-vim.g.nvim_tree_hide_dotfiles = 1 -- 0 by default, this option hides files and folders starting with a dot `.`
-vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
-vim.g.nvim_tree_follow = 1 -- 0 by default, this option allows the cursor to be updated when entering a buffer
-vim.g.nvim_tree_auto_close = 1 -- 0 by default, closes the tree when it's the last window
-vim.g.nvim_tree_auto_ignore_ft = 'startify' -- empty by default, don't auto open tree on specific filetypes.
--- vim.g.nvim_tree_ignore = [ '.git', 'node_modules', '.cache' ] --empty by default
-vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
+ ]]
 
+local g = vim.g
+
+ vim.o.termguicolors = true
+
+g.nvim_tree_side = "left"
+g.nvim_tree_width = 30
+g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
+g.nvim_tree_auto_open = 1
+g.nvim_tree_auto_close = 0
+g.nvim_tree_quit_on_open = 0
+g.nvim_tree_follow = 1
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_hide_dotfiles = 1
+g.nvim_tree_git_hl = 1
+g.nvim_tree_root_folder_modifier = ":t"
+g.nvim_tree_tab_open = 0
+g.nvim_tree_allow_resize = 1
+g.nvim_tree_lsp_diagnostics = 1
+g.nvim_tree_auto_ignore_ft = { "startify", "dashboard" }
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 vim.g.nvim_tree_bindings = {
