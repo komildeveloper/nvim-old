7fdf4912e0ce25680032d87698241a4c253d698c
komildeveloper <komildeveloper111@gmail.com>
2021-07-07 15:49:28 +0500

    update python-ls.lua

--- lua/lsp/python-ls.lua
+++ lua/lsp/python-ls.lua
@@ -1,2 +1,10 @@
 -- npm i -g pyright
-require'lspconfig'.pyright.setup {on_attach = require'lsp'.common_on_attach}
+print(DATA_PATH);
+
+require'lspconfig'.pyright.setup {
+    cmd = {
+        DATA_PATH .. "/lspinstall/python/node_modules/.bin/pyright-langserver",
+        "--stdio"
+    },
+    on_attach = require'lsp'.common_on_attach
+}
